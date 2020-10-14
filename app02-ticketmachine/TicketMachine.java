5a43b3634c236e300b3205ffe091e23dc413a500
Mauro Nunes <21815118@bucks.ac.uk>
2020-10-14 14:49:32 +0100

    added 1 ticket

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -15,30 +15,23 @@
  */
 public class TicketMachine
 {
-    // The price of a ticket from this machine.
-    private int price;
     // The amount of money entered by a customer so far.
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-    
+
+    private Ticket aylesburyTicket;
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
-        price = cost;
         balance = 0;
         total = 0;
+
+        aylesburyTicket = new Ticket("Aylesbury", 220);
     }
 
-    /**
-     * Return The price of a ticket.
-     */
-    public int getPrice()
-    {
-        return price;
-    }
 
     /**
      * Return The amount of money already inserted for the
@@ -117,13 +110,14 @@ public class TicketMachine
      */
     public void printTicket()
     {
+        int price = 500;
+        balance = 500;
         if(balance >= price)
         {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
             System.out.println("##################");
             System.out.println();
 
