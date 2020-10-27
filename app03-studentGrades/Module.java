0b6da0dfda53b0da24a757319728d4e36da9937a
Mauro Nunes <21815118@bucks.ac.uk>
2020-10-27 21:12:56 +0000

    created module class, along with its fields and constructor

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    private String title;
+    
+    private String moduleCode;
+    
+    private int percentageMark;
+    
+    /**
+     * Create a module with a title, code and percentage mark
+     */
+    public Module (String title, String moduleCode, int percentageMark)
+    {
+        this.title = title;
+        this.moduleCode = moduleCode;
+        this.percentageMark = percentageMark;
+    }
+}
